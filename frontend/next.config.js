2593c8c3a89671e1126b3261a254e0fbc73e1fae
hrfm1623 <hiro3510enic@gmail.com>
2024-12-31 16:50:40 +0900

    change: output standalone

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,41 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: ".next",
-  experimental: {},
+  output: "standalone",
   images: {
     unoptimized: true,
-  },
-  compress: true,
-  webpack: (config, { isServer }) => {
-    // プロダクションビルドの最適化
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        chunks: "all",
-        maxInitialRequests: 25,
-        minSize: 20000,
-        maxSize: 24000000, // 24MB未満に制限
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          commons: {
-            name: "commons",
-            chunks: "all",
-            minChunks: 2,
-          },
-          lib: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )[1];
-              return `npm.${packageName.replace("@", "")}`;
-            },
-            chunks: "all",
-          },
-        },
-      };
-    }
-    return config;
   },
 };
 
