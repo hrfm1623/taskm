ce4ff5172bb83abf750d10d2b9108a40d17d394f
hrfm1623 <hiro3510enic@gmail.com>
2024-12-31 21:04:04 +0900

    simple build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  distDir: ".next",
+  output: "export",
   images: {
     unoptimized: true,
   },
   experimental: {
     optimizePackageImports: ["@heroicons/react", "@radix-ui/react-icons"],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
   },
 };
 
