3c27f0f0a67ad7994392afddeb1eba0a66b818d0
hrfm1623 <hiro3510enic@gmail.com>
2024-12-31 16:31:33 +0900

    change: next config commitもれ　

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  distDir: ".vercel/output/static",
+  distDir: ".next",
   experimental: {},
   images: {
     unoptimized: true,
