661e2746bccc67429c927760853524c4ec349aad
hrfm1623 <hiro3510enic@gmail.com>
2024-12-31 16:49:37 +0900

    test config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,38 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  compress: true,
+  webpack: (config, { isServer }) => {
+    // プロダクションビルドの最適化
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: "all",
+        maxInitialRequests: 25,
+        minSize: 20000,
+        maxSize: 24000000, // 24MB未満に制限
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          commons: {
+            name: "commons",
+            chunks: "all",
+            minChunks: 2,
+          },
+          lib: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `npm.${packageName.replace("@", "")}`;
+            },
+            chunks: "all",
+          },
+        },
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
