4904fa6556af36129bcc6075a2e3bfb9ff8ebe49
hirofumi <hiro3510enic@gmail.com>
2025-01-27 12:16:27 +0900

    feat: build process

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,43 +1,13 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    optimizePackageImports: [
-      "@heroicons/react",
-      "@radix-ui/react-icons",
-      "lucide-react",
-      "@radix-ui/react-dialog",
-      "@radix-ui/react-dropdown-menu",
-      "@radix-ui/react-select",
-      "@radix-ui/react-toast",
-    ],
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // 本番環境でのみ適用される最適化
-      config.optimization.minimize = true;
-      config.optimization.splitChunks = {
-        chunks: "all",
-        minSize: 20000,
-        maxSize: 244000,
-        cacheGroups: {
-          defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            reuseExistingChunk: true,
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-        },
-      };
-    }
+  webpack: (config) => {
+    config.cache = false;
     return config;
-  },
+  }
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
