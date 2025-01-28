bc9b06c3d16d12ade91b1ac30d4513ca134f7b97
hirofumi <hiro3510enic@gmail.com>
2025-01-28 15:01:03 +0900

    CF optim

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,22 @@ const nextConfig = {
   webpack: (config) => {
     config.cache = false;
     return config;
-  }
+  },
+  // Cloudflare Pages用の設定
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  // Cloudflare Pages用の追加設定
+  experimental: {
+    isrMemoryCacheSize: 0,
+  },
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
